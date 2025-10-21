aeecbcf88c337510999fb42eea2959ac3bf729f7
Hassaan Ahmed <hassaanahmedc@gmail.com>
2025-10-21 22:58:20 +0500

    made adjustments to searchService to return aditional fields on users table for results page. Fixed the issue where comments and likes counts were not visible on results page. Improved database queries by replacing map function with ->withExists function of laravel on. Made searchController return html response instead of arrays for scalability.

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 Class SearchService {
     
@@ -24,14 +25,14 @@ Class SearchService {
 
     public function searchUser(string $query, array $filters) 
     {
-        $builder = User::query()->select(['id', 'fname', 'lname', 'avatar'])
+        $builder = User::query()->select(['id', 'fname', 'lname', 'avatar', 'bio', 'location'])
             ->where(function ($w) use ($query) {
                 $w->where('fname', 'like', "%{$query}%")
                     ->orWhere('lname', 'like', "%{$query}%");
         });
         
-        if ($filters['near'] && auth()->check() && auth()->user()->city) {
-            $builder->where('city', auth()->user()->city);
+        if ($filters['near'] && auth()->check() && auth()->user()->location) {
+            $builder->where('city', auth()->user()->location);
         }
         
         $users = $builder->limit(10)->get();
@@ -40,9 +41,13 @@ Class SearchService {
     }
 
     public function searchPost(string $query) 
-    {
+    { 
+        $user_id = Auth::id();
         $builder = Post::query()->select(['id', 'user_id', 'content', 'created_at'])
-            ->where('content', 'like', "%{$query}%")->with('user');
+            ->where('content', 'like', "%{$query}%")->with('user')->withCount(['likes', 'comment'])
+            ->withExists(['likes as liked_by_user' => function ($query) use ($user_id) {
+                $query->where('user_id', $user_id);
+            }]);
         
         $posts = $builder->limit(10)->get();
         $posts->each(fn ($p) => $p->type = 'post');
