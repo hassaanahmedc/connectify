95fcd81a3a255d44279abfc23a94e79872bd6b0b
Hassaan Ahmed <hassaanahmedc@gmail.com>
2025-11-15 13:55:45 +0500

    (part of previous commit): added API Endpoints for Follow Funcitonality

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\searchController;
 use App\Http\Controllers\Post\LikeController;
 use App\Http\Controllers\Post\CommentController;
 use App\Http\Controllers\Post\PostController;
+use App\Http\Controllers\FollowController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 /*
@@ -39,13 +40,18 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::delete('/post/{post}/destroy', [PostController::class, 'destroy'])->name('post.destroy');
     Route::put('/post/{post}/update', [PostController::class, 'update'])->name('post.update');
     Route::post('/post/{post}/like', [LikeController::class, 'store'])->name('post.like');
+    // Comment Routes
     Route::post('/post/{post}/comment', [CommentController::class, 'store'])->name('post.comment');
     Route::delete('/post/{comment}/delete', [CommentController::class, 'destroy'])->name('comment.delete');
     Route::patch('/post/{comment}/update', [CommentController::class, 'update'])->name('comment.update');
-    
+    // Follow Routes
+    Route::post('/user/{user}/follow', [FollowController::class, 'toggleFollow'])->name('user.follow');
+    Route::get('/user/{user}/followers', [FollowController::class, 'follower'])->name('user.followers');
+    Route::get('/user/{user}/following', [FollowController::class, 'following'])->name('user.following');
+
 });
-Route::get('/search/results', [searchController::class, 'navSearch'])->name('search.results');
 // Non Protected Public routes
+Route::get('/search/results', [searchController::class, 'navSearch'])->name('search.results');
 Route::get('/post/{post}', [PostController::class, 'show'])->name('post.view');
 Route::get('/post/{post}/viewcomments', [CommentController::class, 'loadMore']);
 
