87fbb63733833d2f0bc46d3065f6ee6b5bd29d23
Hassaan Ahmed <hassaanahmedc@gmail.com>
2026-05-15 16:51:12 +0500

    feat: migrate database to TiDB Cloud and enable SSL encryption

--- config/database.php
+++ config/database.php
@@ -59,7 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => base_path(env('MYSQL_ATTR_SSL_CA')),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY', true),
             ]) : [],
         ],
 
